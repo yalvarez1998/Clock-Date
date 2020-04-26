f6ac438ebc9d0594ebda08ac7607eec885cf34e1
YITZHAK ALVAREZ <yitzhak.alvarez1998@yahoo.com>
2020-04-26 13:59:41 -0400

    commit changes

--- date_time.js
+++ date_time.js
@@ -1,28 +1,35 @@
 function date_time(id)
 {
-    date = new Date;
-    year = date.getFullYear();
-    month = date.getMonth();
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var ms = date.getMilliseconds();
+    var session = "AM";
+
     months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
     d = date.getDate();
     day = date.getDay();
     days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-    h = date.getHours();
-    if(h<10)
+
+    if (h == 0)
     {
-        h = "0"+h;
+        h = 12;
     }
-    m = date.getMinutes();
-    if(m<10)
+    if (h > 12)
     {
-        m = "0"+m;
+        h = h - 12;
+        session = "PM";
     }
-    s = date.getSeconds();
-    if(s<10)
-    {
-        s = "0"+s;
-    }
-    result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    ms = (ms < 10) ? "0" + ms : ms;
+
+    result = " " + days[day] + ',' + " " + months[month] + " " + d + " " + year + " " + h + ':' + m + ':' + s + ':' + ms + " " + session;
     document.getElementById(id).innerHTML = result;
     setTimeout('date_time("'+id+'");','1000');
     return true;
